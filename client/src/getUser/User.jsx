cdedc96101f843fc8e585bb6a7fe79b92be39e46
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-07-13 00:19:57 +0530

    styling of the table in the User.jsx  via the User.css

--- client/src/getUser/User.jsx
+++ client/src/getUser/User.jsx
@@ -1,10 +1,38 @@
 // we will be creating a functional component 
 
 import React from 'react'
+import "./user.css";
 
 const User = () => {
   return (
-    <div>User Table</div>
+    <div className='userTable'>
+      <button type="button" class="btn btn-primary">Add User</button>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th scope='col'>
+              Serial No.
+            </th>
+            <th scope='col'>Name</th>
+            <th scope='col'>Email</th>
+            <th scope='col'>Address</th>
+            <th scope='col'>Actions</th>
+            {/* Actions - for buttons  */}
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>John</td>
+            <td>john@gmail.com</td>
+            <td>Canada</td>
+            <td>Update | Delete</td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
   )
 }
 
