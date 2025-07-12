896ff4fcb8065bc3e7857dacbaf21140450ed9d9
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-07-12 21:56:07 +0530

    User.jsx added - User is a functional component

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import User from "./getUser/User";
+
+function App() {
+  return (
+    <div className="App">
+      <User/>
+    </div>
+  );
+}
+
+export default App;
+
+// we dont need App.test.js - so deleting it
+// also logo.svg
+// reportWeb , setupVItals also
+// we dont need index.css also 
+
+
+// all the dependencies can be found from npmJS website  
---
+++ client/src/getUser/User.jsx
@@ -0,0 +1,13 @@
+// we will be creating a functional component 
+
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>User Table</div>
+  )
+}
+
+export default User;
+// this above const User -- it is a functional component 
+// we need to add this User.jsx into the App.js 
