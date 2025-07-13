4f9e89bdeb61d9edb7a74b5b46f7e4ccbd9da34b
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-07-13 10:04:41 +0530

    font awesome icons are added and axios - which is a js library for http requests are added

--- client/src/getUser/User.jsx
+++ client/src/getUser/User.jsx
@@ -1,24 +1,24 @@
-// we will be creating a functional component 
+// we will be creating a functional component
 
-import React from 'react'
+import React from "react";
 import "./user.css";
 
 const User = () => {
   return (
-    <div className='userTable'>
-      <button type="button" class="btn btn-primary">Add User</button>
-      <table className='table table-bordered'>
+    <div className="userTable">
+      <button type="button" class="btn btn-primary">
+        Add User
+        <i class="fa-solid fa-person-circle-plus addUserIcon"></i>
+      </button>
+      <table className="table table-bordered">
         <thead>
           <tr>
-            <th scope='col'>
-              Serial No.
-            </th>
-            <th scope='col'>Name</th>
-            <th scope='col'>Email</th>
-            <th scope='col'>Address</th>
-            <th scope='col'>Actions</th>
+            <th scope="col">Serial No.</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Address</th>
+            <th scope="col">Actions</th>
             {/* Actions - for buttons  */}
-
           </tr>
         </thead>
         <tbody>
@@ -27,15 +27,24 @@ const User = () => {
             <td>John</td>
             <td>john@gmail.com</td>
             <td>Canada</td>
-            <td>Update | Delete</td>
+            <td className="actionButtons">
+              <button type="button" class="btn btn-warning">
+                <i class="fa-solid fa-wrench"></i>
+              </button>
+              <button type="button" class="btn btn-danger">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </td>
           </tr>
         </tbody>
-
       </table>
     </div>
-  )
-}
+  );
+};
 
 export default User;
-// this above const User -- it is a functional component 
-// we need to add this User.jsx into the App.js 
+// this above const User -- it is a functional component
+// we need to add this User.jsx into the App.js
+
+// now we want the dynamic data from the db 
+// axios - js library for making http requests 
