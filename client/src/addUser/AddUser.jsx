7fba545843615eade9fd326a3585c64648bc990d
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-07-13 11:30:49 +0530

    AddUserForm.jsx page is made also the css portion for this page is also done

--- client/src/addUser/AddUser.jsx
+++ client/src/addUser/AddUser.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import "./AddUser.css";
+import { Link } from "react-router-dom";
 
 const AddUser = () => {
   return (
-    <div>AddUser form</div>
-  )
-}
+    <div className="addUser">
+      <Link to="/" type="button" class="btn btn-success">
+        <i class="fa-solid fa-house"></i> Back
+      </Link>
 
-export default AddUser
+      <h3>Add New User</h3>
+      <form className="addUserForm">
+        <div className="inputGroup">
+          <label htmlFor="name">Name : </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="off"
+            placeholder="Enter your Name"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="email">E-mail : </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="off"
+            placeholder="Enter your Email"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="address">Address : </label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            autoComplete="off"
+            placeholder="Enter your Address"
+          />
+        </div>
+        <div className="inputGroup">
+          <button type="button" class="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
